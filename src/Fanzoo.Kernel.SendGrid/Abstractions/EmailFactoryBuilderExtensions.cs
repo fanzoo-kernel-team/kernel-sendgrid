f6640902952a3cfdcf291d19f75c0f46b7a1aac7
Bill Wheelock <mrbildo@mrbildo.net>
2023-04-12 22:16:36 -0400

    Updates to use new Kernel email factory

---
+++ src/Fanzoo.Kernel.SendGrid/Abstractions/EmailFactoryBuilderExtensions.cs
@@ -0,0 +1,21 @@
+﻿using System.Reflection;
+using Fanzoo.Kernel.Builder;
+using Fanzoo.Kernel.DependencyInjection;
+using Fanzoo.Kernel.SendGrid.Services;
+using Fanzoo.Kernel.Services;
+using Microsoft.AspNetCore.Builder;
+
+namespace Fanzoo.Kernel.SendGrid
+{
+    public static class EmailFactoryBuilderExtensions
+    {
+        public static EmailFactoryBuilder AddSendGrid(this EmailFactoryBuilder builder)
+        {
+            builder.WebApplicationBuilder.AddTransient<IEmailService, SendGridEmailService>();
+
+            builder.WebApplicationBuilder.AddSetting<SendGridSettings>(SendGridSettings.SectionName);
+
+            return builder;
+        }
+    }
+}
